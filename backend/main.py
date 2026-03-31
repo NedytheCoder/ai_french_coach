b52a8e9172b57a5f048c45ef8b113aee666f33da
Nedy King <nedyking007@gmail.com>
2026-03-31 11:16:47 +0100

    Added UI components and refactored the backend prompts

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import tempfile
 
 load_dotenv()
 
@@ -11,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://100.69.122.1:3000"],
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
@@ -19,12 +20,36 @@ app.add_middleware(
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+backendMessages = [
+    {
+        "role": "system",
+        "content": "If the user asks you to translate something in French or in english, translate it directly and explain why it's like that in french in a simple and easy to understand way.",
+    },
+    {
+        "role": "system",
+        "content": "If the user ever specify his/her level (A1, A2, B1, B2, C1, C2), make sure to adjust your language complexity accordingly.",
+    },
+    {
+        "role": "system",
+        "content": "If the user says anything along the lines of 'I do not understand the french sentence or word or phrase' up to 3 times in the conversation, politely ask what level the user is in for you to adjust appropriately",
+    },
+    {
+        "role": "system",
+        "content": "If the user uses English, respond in English but nudge and encourage them to use French next time or more frequently for them to improve.",
+    },
+    {
+        "role": "system",
+        "content": "Always show enthusiasm and encouragement.",
+    },
+]
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from backend!"}
 
 
+# Function to respond to chat with the user
 @app.post("/respond")
 def respond(data: dict):
     messages = data["message"]
@@ -32,11 +57,11 @@ def respond(data: dict):
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a beginner French tutor who loves to chat with students and in french for them to practice and improve their French skills. Always show enthusiasm and encouragement. If the user uses English, respond in English but nudge and encourage them to use French next time or more frequently for them to improve."},
-            {"role": "system", "content": "If the user asks you to translate something in French or in english, translate it directly and explain why it's like that."},
-            {"role": "system", "content": "If the user ever specify his/her level (A1, A2, B1, B2, C1, C2), make sure to adjust your language complexity accordingly."},
-            {"role": "system", "content": "If the user ever specify his/her goal (e.g. travel, work, study, etc.), make sure to use vocabularies and phrases related and relevant to that goal."},
-            {"role": "system", "content": "If the user says anything along the lines of 'I do not understand the french sentence or word or phrase' up to 3 times in the conversation, politely ask what level the user is in for you to adjust appropriately"},
+            {
+                "role": "system",
+                "content": "You are a beginner French tutor who loves to chat with students and in french for them to practice and improve their French skills.",
+            },
+            *backendMessages,
             *messages,
         ],
     )
@@ -44,6 +69,7 @@ def respond(data: dict):
     return {"reply": response.choices[0].message.content}
 
 
+# Function to assist with self-introductions
 @app.post("/introduction")
 def introduction(data: dict):
     messages = data["message"]
@@ -55,10 +81,11 @@ def introduction(data: dict):
                 "role": "system",
                 "content": "You are a beginner French tutor who is only focused on assisting the user with self-introductions(e.g. name, age, where you're from, etc.). Never change the subject or talk about anything else besides self-introductions. If the user tries to change the subject, gently redirect them back to self-introduction topics.",
             },
-            {"role": "system", "content": "You are a beginner French tutor who loves to help users to become masters at introducing themselves in french. Always show enthusiasm and encouragement. If the user uses English, respond in English but nudge and encourage them to use French next time or more frequently for them to improve."},
-            {"role": "system", "content": "If the user asks you to translate something in French or in english, translate it directly and explain why it's like that."},
-            {"role": "system", "content": "If the user ever specify his/her level (A1, A2, B1, B2, C1, C2), make sure to adjust your language complexity accordingly."},
-            {"role": "system", "content": "If the user says anything along the lines of 'I do not understand the french sentence or word or phrase' up to 3 times in the conversation, politely ask what level the user is in for you to adjust appropriately"},
+            {
+                "role": "system",
+                "content": "You are a beginner French tutor who loves to help users to become masters at introducing themselves in french.",
+            },
+            *backendMessages,
             *messages,
         ],
     )
@@ -66,6 +93,7 @@ def introduction(data: dict):
     return {"reply": response.choices[0].message.content}
 
 
+# Function to assist with traveling
 @app.post("/traveling")
 def traveling(data: dict):
     messages = data["message"]
@@ -77,10 +105,11 @@ def traveling(data: dict):
                 "role": "system",
                 "content": "You are a beginner French tutor who is only focused on assisting the user with traveling(e.g. asking for directions, ordering food, airport, etc.). Never change the subject or talk about anything else besides traveling. If the user tries to change the subject, gently redirect them back to traveling topics.",
             },
-            {"role": "system", "content": "You are a beginner French tutor who loves to help users to become masters at knowing what to say when traveling in french. Always show enthusiasm and encouragement. If the user uses English, respond in English but nudge and encourage them to use French next time or more frequently for them to improve."},
-            {"role": "system", "content": "If the user asks you to translate something in French or in english, translate it directly and explain why it's like that."},
-            {"role": "system", "content": "If the user ever specify his/her level (A1, A2, B1, B2, C1, C2), make sure to adjust your language complexity accordingly."},
-            {"role": "system", "content": "If the user says anything along the lines of 'I do not understand the french sentence or word or phrase' up to 3 times in the conversation, politely ask what level the user is in for you to adjust appropriately"},
+            {
+                "role": "system",
+                "content": "You are a beginner French tutor who loves to help users to become masters at knowing what to say when traveling in french.",
+            },
+            *backendMessages,
             *messages,
         ],
     )
@@ -88,6 +117,7 @@ def traveling(data: dict):
     return {"reply": response.choices[0].message.content}
 
 
+# Function to assist with daily conversations
 @app.post("/daily_conversations")
 def daily_conversations(data: dict):
     messages = data["message"]
@@ -99,12 +129,34 @@ def daily_conversations(data: dict):
                 "role": "system",
                 "content": "You are a beginner French tutor who is only focused on assisting the user with daily conversations(e.g. hobbies, weather, family, routine, etc.). Never change the subject or talk about anything else besides daily conversations. If the user tries to change the subject, gently redirect them back to daily conversation topics.",
             },
-            {"role": "system", "content": "You are a beginner French tutor who loves to help users masters at daily conversations in french. Always show enthusiasm and encouragement. If the user uses English, respond in English but nudge and encourage them to use French next time or more frequently for them to improve."},
-            {"role": "system", "content": "If the user asks you to translate something in French or in english, translate it directly and explain why it's like that."},
-            {"role": "system", "content": "If the user ever specify his/her level (A1, A2, B1, B2, C1, C2), make sure to adjust your language complexity accordingly."},
-            {"role": "system", "content": "If the user says anything along the lines of 'I do not understand the french sentence or word or phrase' up to 3 times in the conversation, politely ask what level the user is in for you to adjust appropriately"},
+            {
+                "role": "system",
+                "content": "You are a beginner French tutor who loves to help users become masters at daily conversations(e.g. hobbies, weather, family, routine, etc.) in french.",
+            },
+            *backendMessages,
             *messages,
         ],
     )
 
     return {"reply": response.choices[0].message.content}
+
+
+# Function to transcribe audio
+@app.post("/transcribe")
+def transcribe(file: UploadFile = File(...)):
+    # Save uploaded file to a temporary location
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as tmp:
+        tmp.write(file.file.read())
+        tmp_path = tmp.name
+
+    try:
+        # Open the temporary file and transcribe
+        with open(tmp_path, "rb") as audio_file:
+            transcription = client.audio.transcriptions.create(
+                model="gpt-4o-transcribe", file=audio_file
+            )
+
+        return {"transcription": transcription.text}
+    finally:
+        # Clean up temporary file
+        os.unlink(tmp_path)
---
+++ frontend/app/components/Button.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+    children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
+    onPointerDown?: () => void;
+    onPointerUp?: () => void;
+    onPointerCancel?: () => void;
+    disabled?: boolean;
+}
+
+export default function Button({ children, className, onClick, onPointerDown, onPointerUp, onPointerCancel, disabled }: ButtonProps) {
+    return (
+        <button className={` ${className} cursor-pointer bg-[#007bff] text-white p-3 rounded-full`} onClick={onClick} onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerCancel={onPointerCancel} disabled={disabled}>
+            {children}
+        </button>
+    )
+}
